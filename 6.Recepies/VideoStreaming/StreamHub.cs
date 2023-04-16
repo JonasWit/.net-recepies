f53e081790018fcd486a5e953223291c7c75d70d
JW <jonasz.witek@pm.me>
2023-04-16 14:27:09 +0200

    video updates

--- 6.Recepies/VideoStreaming/StreamHub.cs
+++ 6.Recepies/VideoStreaming/StreamHub.cs
@@ -1,54 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SignalR;
 
-namespace VideoStreaming
+namespace VideoStreaming;
+
+public class StreamHub : Hub
 {
-    public class StreamHub : Hub
+    private readonly IWebHostEnvironment _env;
+
+    public StreamHub(IWebHostEnvironment env) => _env = env;
+
+    public readonly struct VideoData
     {
-        private readonly IWebHostEnvironment _env;
+        public int Index { get; }
+        public string Part { get; }
+        public string UserName { get; }
 
-        public StreamHub(IWebHostEnvironment env)
+        [JsonConstructor]
+        public VideoData(int index, string part, string userName) => (Index, Part, UserName) = (index, part, userName);
+    }
+
+    public async Task SendVideoData(IAsyncEnumerable<VideoData> videoData)
+    {
+        await foreach (var d in videoData)
         {
-            _env = env;
+            await Clients.Others.SendAsync("video-data", d);
         }
+    }
 
-        public struct VideoData
+    public async Task Send()
+    {
+        var c = 0;
+        while (c < 6)
         {
-            public int Index { get; }
-            public string Part { get; }
-
-
-            [JsonConstructor]
-            public VideoData(int index, string part) => (Index, Part) = (index, part);
+            var bytes = GetFile(c++ % 3);
+            await Clients.All.SendAsync("video-data", bytes);
+            await Task.Delay(4000);
         }
+    }
 
-        public async Task SendVideoData(IAsyncEnumerable<VideoData> videoData)
-        {
-            await foreach (var d in videoData)
-            {
-                await Clients.Others.SendAsync("video-data", d);
-            }
-        }
-
-        public async Task Send()
-        {
-            int c = 0;
-            while (c < 6)
-            {
-                var bytes = GetFile(c++ % 3);
-                await Clients.All.SendAsync("video-data", bytes);
-                await Task.Delay(4000);
-            }
-        }
-
-        public byte[] GetFile(int index)
-        {
-            var path = Path.Combine(_env.WebRootPath, $"vid{index}.webm");
-            return File.ReadAllBytes(path);
-        }
+    public byte[] GetFile(int index)
+    {
+        var path = Path.Combine(_env.WebRootPath, $"vid{index}.webm");
+        return File.ReadAllBytes(path);
     }
 }
