1618b48575ddf8ff003461ed271bdb5ab7301976
JW <jonasz.witek@pm.me>
2023-05-21 13:03:59 +0200

    updates

--- 6.Recepies/CookiesAndTokens/KeyManager.cs
+++ 6.Recepies/CookiesAndTokens/KeyManager.cs
@@ -1,5 +1,21 @@
-﻿namespace CookiesAndTokens;
+﻿using System.Security.Cryptography;
+
+namespace CookiesAndTokens;
 
 public class KeyManager
 {
+    public RSA RsaKey { get; }
+    public KeyManager()
+    {
+        RsaKey = RSA.Create();
+        if (File.Exists("key"))
+        {
+            RsaKey.ImportRSAPrivateKey(File.ReadAllBytes("key"), out _);
+        }
+        else
+        {
+            var privateKey = RsaKey.ExportRSAPrivateKey();
+            File.WriteAllBytes("key", privateKey);
+        }
+    }
 }
