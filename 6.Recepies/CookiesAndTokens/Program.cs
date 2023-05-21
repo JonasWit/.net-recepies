5a931e38e1594061ef79a04a5e99e5e770a79473
JW <jonasz.witek@pm.me>
2023-05-21 16:42:53 +0200

    updates

--- 6.Recepies/CookiesAndTokens/Program.cs
+++ 6.Recepies/CookiesAndTokens/Program.cs
@@ -1,4 +1,6 @@
 using CookiesAndTokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -9,8 +11,8 @@ using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyManager = new KeyManager();
-builder.Services.AddSingleton(keyManager);
+var km = new KeyManager();
+builder.Services.AddSingleton(km);
 builder.Services.AddDbContext<IdentityDbContext>(c => c.UseInMemoryDatabase("my_db"));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(o =>
@@ -34,27 +36,61 @@ builder.Services.AddAuthentication()
             ValidateIssuer = false,
         };
 
+        o.Events = new JwtBearerEvents()
+        {
+            OnMessageReceived = ctx =>
+            {
+                if (ctx.Request.Query.TryGetValue("t", out var token))
+                {
+                    ctx.Token = token;
+                }
+                return Task.CompletedTask;
+            }
+        };
+
         o.Configuration = new OpenIdConnectConfiguration()
         {
             SigningKeys =
             {
-                new RsaSecurityKey(keyManager.RsaKey)
+                new RsaSecurityKey(km.RsaKey)
             }
         };
         o.MapInboundClaims = false;
 
     });
 
-builder.Services.AddAuthorization();
+builder.Services.AddAuthorization(b =>
+{
+    b.DefaultPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .AddAuthenticationSchemes(IdentityConstants.ApplicationScheme, "jwt")
+        .Build();
+
+    b.AddPolicy("the_policy", pb =>
+        pb.RequireAuthenticatedUser()
+        .AddAuthenticationSchemes(IdentityConstants.ApplicationScheme, "jwt")
+        .RequireClaim("role", "janitor"));
+
+    b.AddPolicy("cookie_policy", pb =>
+        pb.RequireAuthenticatedUser()
+        .AddAuthenticationSchemes(IdentityConstants.ApplicationScheme)
+        .RequireClaim("role", "janitor"));
+
+    b.AddPolicy("token_policy", pb =>
+        pb.RequireAuthenticatedUser()
+        .AddAuthenticationSchemes("jwt")
+        .RequireClaim("role", "janitor"));
+
+});
 
 var app = await builder.BuildAndSetup();
 
-app.MapGet("/", (ClaimsPrincipal user) => user.Claims.Select(c => KeyValuePair.Create(c.Type, c.Value)));
+app.MapGet("/", (ClaimsPrincipal user) => user.Claims.Select(c => KeyValuePair.Create(c.Type, c.Value))).RequireAuthorization();
 app.MapGet("/test", () => "test");
 
 app.MapGet("/secret", () => "secret").RequireAuthorization("the_policy");
-app.MapGet("/secret-cookie", () => "cookie secret").RequireAuthorization("the_policy", "cookie_policy");
-app.MapGet("/secret-token", () => "token secret").RequireAuthorization("the_policy", "token_policy");
+app.MapGet("/secret-cookie", () => "cookie secret").RequireAuthorization("cookie_policy");
+app.MapGet("/secret-token", () => "token secret").RequireAuthorization("token_policy");
 
 app.MapGet("/cookie/sign-in", async (SignInManager<IdentityUser> signInManager) =>
 {
@@ -62,18 +98,36 @@ app.MapGet("/cookie/sign-in", async (SignInManager<IdentityUser> signInManager) 
     return Results.Ok();
 });
 
-app.MapGet("/jwt/sign-in", () => (SignInManager<IdentityUser> signInManager) =>
-{
-    var handler = new JsonWebTokenHandler();
-    var key = new RsaSecurityKey(keyManager.RsaKey);
-    var token = handler.CreateToken(new SecurityTokenDescriptor()
+app.MapGet("/jwt/sign-in", async (
+    SignInManager<IdentityUser> signInManager,
+    UserManager<IdentityUser> userManager,
+    KeyManager keyManager,
+    IUserClaimsPrincipalFactory<IdentityUser> userClaimsPrincipalFactory) =>
     {
-        Issuer = "https://localhost:7100",
-        Subject = null,
-        SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256)
-    });
+        var user = await userManager.FindByNameAsync("test@test.com");
+        var result = await signInManager.CheckPasswordSignInAsync(user, "password", false);
+        if (!result.Succeeded)
+        {
+            return Results.BadRequest();
+        }
 
-});
+        var principal = await userClaimsPrincipalFactory.CreateAsync(user);
+
+        var identity = principal.Identities.First();
+        identity.AddClaim(new Claim("amr", "pwd"));
+        identity.AddClaim(new Claim("method", "jwt"));
+
+        var handler = new JsonWebTokenHandler();
+        var key = new RsaSecurityKey(keyManager.RsaKey);
+        var token = handler.CreateToken(new SecurityTokenDescriptor()
+        {
+            Issuer = "https://localhost:7100",
+            Subject = identity,
+            SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256)
+        });
+
+        return Results.Ok(token);
+    });
 
 app.Run();
 
